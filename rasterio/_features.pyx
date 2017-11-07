d46d0be57b3e76952dfe696a697e77a6eef285c4
Frédéric Bonifas <frederic.bonifas@makina-corpus.com>
2017-11-07 12:03:30 +0100

    add a merge_alg_add option to the rasterize feature

--- rasterio/_features.pyx
+++ rasterio/_features.pyx
@@ -256,7 +256,7 @@ def _sieve(image, size, out, mask, connectivity):
         mask_mem_ds.close()
 
 
-def _rasterize(shapes, image, transform, all_touched):
+def _rasterize(shapes, image, transform, all_touched, merge_alg_add):
     """
     Burns input geometries into `image`.
 
@@ -275,6 +275,9 @@ def _rasterize(shapes, image, transform, all_touched):
         If false, only pixels whose center is within the polygon or
         that are selected by Bresenham's line algorithm will be burned
         in.
+    merge_alg_add : boolean, optional
+        If True, the new value will be added to the existing raster. If
+        False, the new value will overwrite the existing value.
     """
 
     cdef int retval
@@ -288,6 +291,8 @@ def _rasterize(shapes, image, transform, all_touched):
     try:
         if all_touched:
             options = _gdal.CSLSetNameValue(options, "ALL_TOUCHED", "TRUE")
+        if merge_alg_add:
+            options = _gdal.CSLSetNameValue(options, "MERGE_ALG", "ADD")
 
         # GDAL needs an array of geometries.
         # For now, we'll build a Python list on the way to building that
